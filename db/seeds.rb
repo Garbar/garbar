a25aba5dfea3a3e2945f6a114849e4c5ba3dc276
Oksana Hamster <lorelgarbar@yahoo.com>
2018-02-27 14:09:41 +0200

    clear seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
 
-require 'database_cleaner'
+# require 'database_cleaner'
 
-DatabaseCleaner.strategy = :truncation, {:except => %w[fandoms authors]}
-DatabaseCleaner.strategy = :truncation
-# then, whenever you need to clean the DB
-DatabaseCleaner.clean
+# DatabaseCleaner.strategy = :truncation
+# # then, whenever you need to clean the DB
+# DatabaseCleaner.clean
 
 imdb_list = %w(tt2487938 tt2819924 tt1936532 tt1129398 tt2294189 tt2396135
               tt1693592 tt1652216 tt1888075 tt1119644 tt0362357 )
